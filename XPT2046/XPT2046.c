472a63d29f87ef0b89b0b6dad881acaed62cad47
Folan <olanrewajufiyin@gmail.com>
2025-01-21 12:55:55 -0500

    Save

--- XPT2046/XPT2046.c
+++ XPT2046/XPT2046.c
@@ -6,6 +6,35 @@
 #include "XPT2046.h"
 
 
+void XPT2046_Init(){
+ControlData InstructionByte; 
+
+
+	InstructionByte.start_bit = XPT2046_START_YES; 
+	InstructionByte.channel_bit = ;
+	InstructionByte.mode_bit = XPT2046_8_BIT_MODE;
+	InstructionByte.reference_bit = XPT2046_SER_REFERENCE;
+	InstructionByte.power_bit;
+
+}
+void XPT2046_DeInit(){
+
+
+}
+
+
+void Read_Data(uint8_t Instruction){
+
+	//TxData = Instruction to send
+	//RxData = Receive Data from 
+	uint8_t TxData = Instruction; 
+	uint8_t RxData = 0;
+
+	
+	HAL_SPI_TransmitReceive( SPI_HANDLE, &TxData, &RxData, 1,  HAL_MAX_DELAY);
+}
+
+
 int main(int argc, char const *argv[])
 {
 	/* code */
--- XPT2046/XPT2046.h
+++ XPT2046/XPT2046.h
@@ -1,4 +1,17 @@
 
+
+
+
+// SPI Definitions
+
+#define SPI_HANDLE     // Important -- Spi_handle you are using
+#define
+
+
+
+
+
+
 /* 
  [ S| A2 | A1 | A0 | MODE | SER/DFR | PD1 | PD0 ]
 
@@ -13,8 +26,54 @@
 #define XPT2046_PD1			0x01 << 1
 #define XPT2046_PD0			0x01 << 0
 
+//The start bits
+
+#define XPT2046_START_YES  XPT2046_START_BIT
+#define XPT2046_START_NO	0x00
+
+// The Channel
+		//Ser options
+#define XPT2046_SER_TEMP0_POSITION		0x00
+#define XPT2046_SER_VBAT_POSITION		XPT2046_A1
+#define XPT2046_SER_AUXIN_POSITION		XPT2046_A2 | XPT2046_A1
+#define XPT2046_SER_TEMP1_POSITION		XPT2046_A2 | XPT2046_A1 | XPT2046_A0
 
 
+#define XPT2046_SER_Y_POSITION			XPT2046_A0
+#define XPT2046_SER_X_POSITION 			XPT2046_A2 | XPT2046_A0
+#define XPT2046_SER_Z1_POSITION			XPT2046_A1 | XPT2046_A0
+#define XPT2046_SER_Z2_POSITION			XPT2046_A2 
+
+		//Dfr options
+#define XPT2046_DFR_Y_POSITION			XPT2046_A0
+#define XPT2046_DFR_X_POSITION			XPT2046_A2 | XPT2046_A0
+#define XPT2046_DFR_Z1_POSITION			XPT2046_A1 | XPT2046_A0
+#define XPT2046_DFR_Z2_POSITION			XPT2046_A2 
+
+
+// The mode
+#define XPT2046_8_BIT_MODE  	XPT2046_MODE
+#define XPT2046_12_BIT_MODE		0x00
+
+//Reference options
+#define XPT2046_SER_REFERENCE	XPT2046_REFERENCE
+#define XPT2046_DFR_REFERENCE 	0x00
+
+//Power Bits
+#define XPT2046_POWER_OFF			0x00
+#define XPT2046_REF_OFF_ADC_ON		XPT2046_PD0 	
+#define XPT2046_REF_ON_ADC_OFF		XPT2046_PD1
+#define XPT2046_DEVICE_ON			XPT2046_PD0 | XPT2046_PD1
+
+
+typedef struct ControlData{
+	uint8_t start_bit ;
+	uint8_t channel_bit; 
+	uint8_t mode_bit;
+	uint8_t reference_bit;
+	uint8_t power_bit;
+
+} Control_Data;
 
 
 // Function Declarations
